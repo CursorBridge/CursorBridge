c353a90e977f285ec828c726e27492ac6992454b
Rangerdevv <meerabdul2008@gmail.com>
2022-03-13 20:16:30 -0400

    deleted images

--- Components/icons/controller.js
+++ Components/icons/controller.js
@@ -1,26 +1,12 @@
 // Buttons
-const HTML = document.getElementById("HTML");	
-const CSS = document.getElementById("CSS");
+
 const SVG = document.getElementById("SVG");
 const IMG = document.getElementById("IMG");
 // contents 
-const HTMLcontent = document.getElementById("HTMLcontent");
-const CSScontent = document.getElementById("CSScontent");
+
 const SVGcontent = document.getElementById("SVGcontent");
 const IMGcontent = document.getElementById("IMGcontent");
 
-HTML.onclick = function(){
-    HTMLcontent.style.display = "block";
-    CSScontent.style.display = "none";
-    SVGcontent.style.display = "none";
-    IMGcontent.style.display = "none";
-}
-CSS.onclick = function(){
-    HTMLcontent.style.display = "none";
-    CSScontent.style.display = "block";
-    SVGcontent.style.display = "none";
-    IMGcontent.style.display = "none";
-}
 SVG.onclick = function(){
     HTMLcontent.style.display = "none";
     CSScontent.style.display = "none";
---
+++ update.sh
@@ -0,0 +1,6 @@
+# bin/bash
+echo "Updating..."
+git add .
+git commit -m "deleted images"
+git push 
+echo "Done"
